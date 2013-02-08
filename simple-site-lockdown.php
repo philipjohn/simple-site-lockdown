eae006256618763d0bb9187b311a2f9c8b827ec4
Philip John <phil@philipjohn.co.uk>
2013-02-08 19:11:12 +0000

    version 1.1 - see readme

--- simple-site-lockdown.php
+++ simple-site-lockdown.php
@@ -3,27 +3,15 @@
 Plugin Name: Simple Site Lockdown
 Plugin URI: http://philipjohn.co.uk/category/plugins/simple-site-lockdown/
 Description: Provides a really simple mechanism for locking down a site so that it's private to all but logged in admin users.
-Version: 1.0
+Version: 1.1
 Author: Philip John
 Author URI: http://philipjohn.co.uk
-License: GPL2
-
-    Copyright (C) 2012 Philip John Ltd
-
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+License: WTFPL
 */
+
+// Initial sanity check
+if (! defined('ABSPATH'))
+	die('Please do not directly access this file');
 
 /*
  * Localise the plugin
